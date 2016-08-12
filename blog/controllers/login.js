28fb9530a092e49a77ba566ce0cce6604f619a26
Donato <cruzmdonato@gmail.com>
2016-08-12 03:08:02 -0400

    added modefied login controller.

---
+++ blog/controllers/login.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const models = require('../models');
+const passport = require('../middlewares/auth');
+
+module.exports = {
+  registerRouter() {
+    const router = express.Router();
+
+    router.get('/', this.index);
+    router.post('/', this.login);
+
+    return router;
+  },
+  index(req, res) {
+    res.render('login');
+  },
+  login(req, res) {
+    passport.authenticate('local', {
+      successRedirect: '/profile',
+      failureRedirect: '/login',
+    })(req, res);
+  },
+};
